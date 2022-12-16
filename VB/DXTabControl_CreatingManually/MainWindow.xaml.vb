c5bd7b818c02b2d4f0bf3e7201e350e3779e3e3b
DevExpressExampleBot <fakeEmail>
2022-12-16 21:41:54 +0400

    Source auto update [skip ci]

--- VB/DXTabControl_CreatingManually/MainWindow.xaml.vb
+++ VB/DXTabControl_CreatingManually/MainWindow.xaml.vb
@@ -1,30 +1,29 @@
-﻿Imports Microsoft.VisualBasic
 Imports System.Windows
 Imports System.Windows.Controls
 Imports DevExpress.Xpf.Core
 
 Namespace DXTabControl_CreatingManually
-	Partial Public Class MainWindow
-		Inherits Window
-		Public Sub New()
-			InitializeComponent()
-		End Sub
-		Private Sub LayoutRoot_Loaded(ByVal sender As Object, ByVal e As RoutedEventArgs)
-			Dim tabControl As New DXTabControl()
-			tabControl.Height = 75
-			tabControl.Width = 200
 
-			Dim tabItem1 As New DXTabItem()
-			tabItem1.Header = "Button"
-			tabItem1.Content = New Button() With {.Content = "Button"}
-			tabControl.Items.Add(tabItem1)
+    Public Partial Class MainWindow
+        Inherits Window
 
-			Dim tabItem2 As New DXTabItem()
-			tabItem2.Header = "Check box"
-			tabItem2.Content = New CheckBox() With {.Content = "Check box"}
-			tabControl.Items.Add(tabItem2)
+        Public Sub New()
+            Me.InitializeComponent()
+        End Sub
 
-			LayoutRoot.Children.Add(tabControl)
-		End Sub
-	End Class
+        Private Sub LayoutRoot_Loaded(ByVal sender As Object, ByVal e As RoutedEventArgs)
+            Dim tabControl As DXTabControl = New DXTabControl()
+            tabControl.Height = 75
+            tabControl.Width = 200
+            Dim tabItem1 As DXTabItem = New DXTabItem()
+            tabItem1.Header = "Button"
+            tabItem1.Content = New Button() With {.Content = "Button"}
+            tabControl.Items.Add(tabItem1)
+            Dim tabItem2 As DXTabItem = New DXTabItem()
+            tabItem2.Header = "Check box"
+            tabItem2.Content = New CheckBox() With {.Content = "Check box"}
+            tabControl.Items.Add(tabItem2)
+            Me.LayoutRoot.Children.Add(tabControl)
+        End Sub
+    End Class
 End Namespace
